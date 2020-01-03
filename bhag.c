712c9f0a71d07284a508e7ed8c6dea510d2fbb6a
Bond01 <aryandubey2389@gmail.com>
2020-01-04 00:36:11 +0530

    Change in thirsty

--- bhag.c
+++ bhag.c
@@ -2,8 +2,13 @@
 int main()
 {
 	int n;
+	scanf("%d",&n);
 	if (n<0)
+	{
 		printf("I'll play cricket\n");
+		printf("I'll play badminton\n");
+		printf("I'll play table tennis\n");
+	}
 	else
 		printf("I'll play football\n");
 	return 0;
